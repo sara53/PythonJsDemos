0a7d4e175c85dedf46ed5e4ef9b70491c83e1851
sara53 <sara.atef001@gmail.com>
2023-03-11 13:37:53 +0200

    day01 demo

---
+++ day01/js/demo.js
@@ -0,0 +1,148 @@
+/** Operator
+ *  -- (+,-,*,/)
+ *  -- (>,< ,>=,<=)
+ *
+ * [==] --> (only values) , [===] (values , datatype)
+ 
+ */
+
+/**
+ * (coercion) --> js engine --> convert automatic from datatype to anther datatype
+ */
+// var a = 2; //== 1
+// var b = "2"; // NaN
+// console.log(a == b);
+
+// var a = "mona";
+
+// a = "ola";
+/** Logical Operator
+ * ---------------------------
+ *
+ * and (&&)
+ *
+ * true && true ==> true
+ * false&& true ==> false
+ * true&&false ==> false
+ * false && false ==> false
+ *
+ * OR , ||
+ *
+ * true || true ==> true
+ * true || false ==> true
+ * false || true ==> true
+ *
+ * false || flase ==> false
+ *
+ *
+ * Not |
+ *
+ * true ==> false
+ * false ==> true
+ *
+ */
+
+// Boolean
+// "mona" && "ola"; //--> "ola"
+// true && true --> true;
+
+// 0 && "ali" --> 0
+// false
+
+// "mona" && "ola";
+
+// true &&  true --> true
+
+/**
+ * if , if else , if else if
+ * switch ,, for , while , do ... while
+ *
+ */
+
+// var a;
+
+// if (a == 3) {
+//   console.log("a == 3");
+// } else if (a == 5) {
+//   console.log("a == 5");
+// } else {
+//   console.log("a !== 3, 5");
+// }
+
+/**
+ *
+ * + --> concat  --> string + string
+ * + --> add , number + number
+ */
+// var a = 2;
+// var b = "5"; // NaN
+
+// console.log(a - b);
+
+// var color = "red";
+
+// switch (color) {
+//   case "red":
+//     console.log("your fav color is : " + color);
+//     break;
+//   case "green":
+//     console.log("your fav color is : " + color);
+//     break;
+//   default:
+//     console.log("you do not have any fav color");
+// }
+
+// for (var i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// var i = 6;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
+
+// var trackName = "python track";
+// var i = 4;
+
+// document.writeln("<h" + i + ">Hello From ITI</h" + i + ">");
+
+// var num1 = prompt("Enter num1");
+// var num2 = prompt("Enter num2");
+// document.writeln(Number(num1) + Number(num2));
+
+// alert("you deleted this item");
+
+// var result = confirm("are you sure you want to delete this item");
+// if (result) {
+//   document.writeln("you deleted this item");
+// } else {
+//   document.writeln("Operation canceld");
+// }
+
+// var userOption;
+
+// do {
+//   userOption = prompt("enter value");
+//   document.writeln("Hello");
+// } while (userOption == "yes");
+
+// var userOption;
+// while (userOption != "yes") {
+//   userOption = prompt("enter value again");
+// }
+// console.log("Finnlly you entered yed");
+// 30
+// 40
+// 50
+// 120
+/** regx in js --> task */
+// ahmed -- 1
+// sara -- 0
+// eeell oo --> 3
---
+++ day01/js/main.js
@@ -0,0 +1,53 @@
+// var fname = "m"; // string
+// var num = 1.5858; // number
+// var flag = true; // boolean
+
+// console.log(typeof fname);
+// console.log(typeof num);
+// console.log(typeof flag);
+// varaible -- camelCase
+
+// var num = 10;
+
+// var num = Number("mona");
+// console.log(num);
+// console.log(typeof num);
+
+// var flag = true;
+
+// var flag = Boolean(false);
+// console.log(flag);
+
+/** NaN --> Not a Number */
+/**4
+ *
+ *
+ * fname = {} emoty Object
+ */
+
+// -------------------
+// undefined ==> var not initalized
+//----------------------
+
+/** Datatype in js
+ * ---------------------
+ * Primitive Dataype
+ * -------------------
+ *    1- string
+ *    2- number
+ *    3- boolean
+ *    4- undefined
+ *    5- null
+ * --------------------------
+ * refernce datatype
+ * --------------------------
+ *  Object
+ *
+ */
+
+/** falsy Values
+ * -------------------
+ * false -- 0 -- undefined -- "" - null - NaN
+ * ---
+ * truthy values
+ */
