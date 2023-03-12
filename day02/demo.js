659c7de6bf1b6a6c5d77e419fef38367874d7396
sara <sara.atef001@gmail.com>
2023-03-12 12:52:59 +0200

    day02

---
+++ day02/demo.js
@@ -0,0 +1,69 @@
+// functions , parameters
+// function add(a, b) {
+//   var result = a + b;
+//   console.log(result);
+//   return undefined;
+// }
+
+// var test = add(4, 6);
+// console.log(test);
+
+// function print() {
+//   for (var i = 0; i < 3; i++) {
+//     console.log(i);
+//   }
+
+//   return "sara";
+// }
+// var test = print();
+// console.log(test);
+// function statement
+// function sayHello(trackName) {
+//   trackName = trackName || "Open source";
+//   return "welcome " + trackName;
+// }
+
+// var test = sayHello("python");
+// console.log(test);
+/**
+ * functions in js
+ * --------------------------
+ *  == built in functions --> funName(parameters)
+ *  == user defined Functions
+ *      -- function statement (function declaration)
+ *      -- function expression
+ */
+/**function statement ,declaration */
+// function add(/**params */) {}
+
+/** function expression  , anonymous function */
+
+// var result = add("welcome ", "mona");
+// console.log(result);
+
+// console.log(a);
+// var a = "mona";
+
+// console.log(a);
+// var a = "mona";
+// console.log(a);
+// var b = "ali";
+
+// console.log(a);
+// var a = "Ali";
+// console.log(a);
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(2, 6);
+
+// var sayHello = function () {
+//   return "welcome from iti";
+// };
+
+// var test = sayHello();
+
+// console.log(test);
+
+//date Object (self study)
+// callback function (search) ...
---
+++ day02/main.js
@@ -0,0 +1,74 @@
+// var str = "Hello from ITI";
+
+// var str = "USERNAME=mona;EMAIL=a@a.com;PASSWORD=123";
+
+/**
+ * String API ----
+ * ---------------------
+ * length
+ * --------------
+ * concat
+ * charAt -->
+ * indexOf
+ * split
+ * includes
+ * startWith
+ * endWith
+ * toUpperCase
+ * toLowercase
+ * -----------------------
+ * map, filter ,every ,some , foreach -- es6
+ */
+
+// console.log(str.toUpperCase());
+
+// console.log(str.endsWith("ggg"));
+
+// console.log(str.includes("@"));
+
+// console.log(cookie.split("er"));
+
+// console.log(cookie.split(";")[0].split("="));
+
+// console.log(str.indexOf("H"));
+// console.log(str.charAt(2));
+
+// console.log(`${str} , ${str2} , welcome at ITI`);
+
+// for (var i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+/**================================= */
+// var arr = ["username=mona", "password=ali"]; // collection of data
+
+/**
+ * push =---- > after last element
+ * unshift --> before first element
+ * pope -->> remove last element
+ * shift --> remove first element
+ * reverse
+ * sort
+ * join
+ *
+ */
+// var firstElement = arr[0];
+// var arr = firstElement.split("=").join();
+
+// console.log(arr.sort().reverse());
+// arr.shift();
+// arr.shift();
+
+// console.log(arr);
+// arr.unshift("new value");
+// arr.unshift(true);
+// console.log(arr);
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   arr.push(i);
+// }
+
+// console.log(arr.reverse());
+// console.log(arr);
