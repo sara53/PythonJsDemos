d6a7338d236b6afeba231bc956178ca1d6b9705f
sara <sara.atef001@gmail.com>
2023-03-13 12:18:49 +0200

    day03 demo

---
+++ day03/main.js
@@ -0,0 +1,108 @@
+/**
+ * functions
+ *  -----------
+ *      built in functions
+ *      user defined
+ *          - function statement
+ *          - function expression
+ * == callback function
+ */
+
+/** params , callbackfunction */
+// function greet(myFun1, fname) {
+//   console.log("Welcome From ITI");
+//   myFun1();
+//   console.log(fname);
+// }
+
+// greet(function () {
+//   console.log("Welcome os track");
+// }, "mona");
+// function greet() {
+//   var result = sayHello();
+//   console.log("hello", result);
+//   return "success";
+// }
+
+// function sayHello() {
+//   console.log("Welcome from ITI");
+//   return "python track";
+// }
+
+// user defined object
+
+// var result = greet();
+// console.log(result);
+// var person = {
+//   fname: "ahmed",
+//   age: 30,
+//   haveCar: true,
+//   address: {
+//     city: "benha",
+//     street: "saad z str",
+//     code: 123,
+//   },
+//   colors: ["red", "green", "blue"],
+//   display: function () {
+//     console.log(person.fname, person.age);
+//   },
+// };
+
+// console.log(person["address"]["code"]);
+
+// var arr = person["colors"][1];
+
+// console.log(arr);
+// console.log(person.display());
+// console.log(person.fname);
+// console.log(person.display);
+// override , not found -- add
+
+// person.disolay = true;
+// console.log(person);
+// set
+// person.fname = "ali";
+// person["fname"] = "alaa";
+// console.log(person);
+// get
+// var test = person.age; //dot notation
+// var test2 = person["age"]; // subscript
+// console.log(test2);
+// console.log(person.color);
+// console.log(test);
+/**Properties --> key : value(any datatype)
+ * methods
+ */
+// access
+/**
+ * Dot Notation
+ * subscriptNotation
+ */
+// console.log(Math.PI);
+/**
+ * PI,
+ * sqrt
+ * min
+ * max
+ * pow
+ * floor
+ * ceil
+ * random
+ * round
+ */
+
+// console.log(Math.sqrt(16));
+// console.log(Math.pow(3, 2));
+// console.log(Math.round(5.4));
+// console.log(Math.floor(5.8));
+// console.log(Math.floor(5.0));
+// console.log(Math.ceil(5.1));
+// console.log(Math.ceil(5.8));
+
+// console.log(Math.max(1, 2, 5, 7, 6, 7, 8, 8));
+// console.log(Math.min(1, 2, 5, 7));
+// console.log(Math.floor(Math.random() * 100));
+
+// console.log(Math.random);
+
+// false ? console.log("True case") : console.log("else case");
